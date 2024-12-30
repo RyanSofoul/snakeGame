6ad51493a86196224e585d7923cff92fd48f97e7
Ryan S <rsofoul@gmail.com>
2024-12-30 11:32:36 -0700

    Add files via upload

---
+++ food.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+import random
+
+
+# Creating the food class from the parent turtle class
+class Food(Turtle):
+
+    # Customizing the food
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    # Moving the food to random coordinates
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
---
+++ snake.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+
+# Sets starting position for the segments as well as how fast and how it can move
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+# Setting up the Snake class
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for positions in STARTING_POSITIONS:
+            self.add_segment(positions)
+
+    def add_segment(self, positions):
+        new_segment = Turtle('square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(positions)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
